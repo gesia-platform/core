0b976cca38df3776a472a780d980c6d035c7b0fc
ash991213 <ads1213@nate.com>
2024-03-19 11:32:46 +0900

    feat(carbon-emissions): Create carbon-emission contract

---
+++ apps/carbon-emission/contracts/calculator/CarbonWaterCalculator.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../core/CarbonEmissions.sol";
+import "./CarbonEmissionsCalculatorBase.sol";
+
+contract CarbonWaterCalculator is CarbonEmissionsCalculatorBase {
+    //수도 탄소배출 계수 0.247kgCO2/m³ scaled by 10 000
+    uint256 public waterCarbonEmissionValue = 2470;
+
+    // calculate CO2 emission by water in (18 decimals)
+    function calculateCO2ByWater(
+        uint256 usedWaterAmount,
+        uint256 applicationId,
+        address carbonEmissions,
+        string memory sourceChannel
+    ) external returns (uint256) {
+        // 수도 사용량(m³) x 수도 탄소배출 계수(kgCO2/m³) = 탄소배출량(kgCO2)
+        uint256 result = waterCarbonEmissionValue * usedWaterAmount;
+
+        uint256 scaledResult = (result * 1e18) / 1e8;
+
+        onCalculated(
+            carbonEmissions,
+            msg.sender,
+            scaledResult,
+            applicationId,
+            sourceChannel
+        );
+
+        return scaledResult;
+    }
+
+    function onCalculated(
+        address carbonEmissions,
+        address from,
+        uint256 amount,
+        uint256 applicationId,
+        string memory sourceChannel
+    ) internal virtual override {
+        CarbonEmissions(carbonEmissions).mint(
+            from,
+            amount,
+            applicationId,
+            sourceChannel
+        );
+    }
+}
