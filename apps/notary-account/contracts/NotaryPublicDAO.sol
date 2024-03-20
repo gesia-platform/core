f19874c6f0b808cfe61e622bff0e21fceaee1ce3
Jin Park <jinpk@users.noreply.github.com>
2024-03-20 12:18:55 +0900

    refactor(notary-account): Make abstract public dao contract

--- apps/notary-account/contracts/NotaryPublicDAO.sol
+++ apps/notary-account/contracts/NotaryPublicDAO.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 /** POA의 Node가 적은 관계로 merkle을 사용하지 않습니다. */
-contract NotaryPublicDAO is Ownable {
+abstract contract NotaryPublicDAO is Ownable {
     using ECDSA for bytes32;
 
     mapping(address => bool) public memberships;
