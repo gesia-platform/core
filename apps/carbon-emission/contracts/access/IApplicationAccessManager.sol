fb6ff0dac9d26320fcc03b745502767c12362a29
Jin Park <jinpk@users.noreply.github.com>
2024-03-24 14:38:42 +0900

    chore: Delete carbon emission application

--- apps/carbon-emission/contracts/access/IApplicationAccessManager.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-interface IApplicationAccessManager {
-    function isAuthorized(
-        uint256 appId,
-        address _address
-    ) external view returns (bool);
-}
--- apps/carbon-emission/contracts/operator/IOperator.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-interface IOperator {
-    function isOperator(address _account) external view returns (bool);
-}
