e99a43804584af7a770a65d5ef338991bd902bea
ash991213 <ads1213@nate.com>
2024-04-08 11:50:02 +0900

    test(contract) : add emission calculator test

--- apps/contract/migrations/6_deploy_test_calculator.js
+++ apps/contract/migrations/6_deploy_test_calculator.js
@@ -4,10 +4,11 @@ const TestAmericanoCarbonEmissionsCalculator = artifacts.require('TestAmericanoC
 module.exports = async (deployer) => {
 	if (process.env.SKIP_MIGRATIONS) return;
 
-	await deployer.deploy(CarbonEmissions, 'TestCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'TestCarbonEmissions', process.env.EMISSION_NOTARY_PUBLIC);
 	const testCarbonEmissions = await CarbonEmissions.deployed();
-	await deployer.deploy(TestAmericanoCarbonEmissionsCalculator, testCarbonEmissions.address, process.env.NOTARY_PUBLIC_ADDRESS);
+	await deployer.deploy(TestAmericanoCarbonEmissionsCalculator, testCarbonEmissions.address, process.env.EMISSION_NOTARY_PUBLIC);
 	const testAmericanoCarbonEmissionsCalculator = await TestAmericanoCarbonEmissionsCalculator.deployed();
+	await testCarbonEmissions.setCalculatorApproval(testAmericanoCarbonEmissionsCalculator.address, true);
 
 	console.log('TestAmericanoCarbonEmissionsCalculator : ', testAmericanoCarbonEmissionsCalculator.address);
 };
