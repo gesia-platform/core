06cc43c4a49f93f97ea85f91138a4a5a0fd05a61
ash991213 <ads1213@nate.com>
2024-04-08 12:21:44 +0900

    feat(contract) : feat calculator contract migration

--- apps/contract/migrations/1_deploy_calculator.js
+++ apps/contract/migrations/1_deploy_calculator.js
@@ -25,115 +25,137 @@ const AmericanoCarbonEmissionsCalculator = artifacts.require('AmericanoCarbonEmi
 module.exports = async (deployer) => {
 	if (process.env.SKIP_MIGRATIONS) return;
 
-	await deployer.deploy(CarbonEmissions, 'BeefCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'BeefCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const beefCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(BeefCarbonEmissionsCalculator, beefCarbonEmissions.address);
 	const beefCarbonEmissionsCalculator = await BeefCarbonEmissionsCalculator.deployed();
+	await beefCarbonEmissions.setCalculatorApproval(beefCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'BicycleCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'BicycleCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const bicycleCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(BicycleCarbonEmissionsCalculator, bicycleCarbonEmissions.address);
 	const bicycleCarbonEmissionsCalculator = await BicycleCarbonEmissionsCalculator.deployed();
+	await bicycleCarbonEmissions.setCalculatorApproval(bicycleCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'CannedTunaCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'CannedTunaCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const cannedTunaCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(CannedTunaCarbonEmissionsCalculator, cannedTunaCarbonEmissions.address);
 	const cannedTunaCarbonEmissionsCalculator = await CannedTunaCarbonEmissionsCalculator.deployed();
+	await cannedTunaCarbonEmissions.setCalculatorApproval(cannedTunaCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'CannedTunaCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'CannedTunaCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const chickenCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(ChickenCarbonEmissionsCalculator, chickenCarbonEmissions.address);
 	const chickenCarbonEmissionsCalculator = await ChickenCarbonEmissionsCalculator.deployed();
+	await chickenCarbonEmissions.setCalculatorApproval(chickenCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'EggCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'EggCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const eggCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(EggCarbonEmissionsCalculator, eggCarbonEmissions.address);
 	const eggCarbonEmissionsCalculator = await EggCarbonEmissionsCalculator.deployed();
+	await eggCarbonEmissions.setCalculatorApproval(eggCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'EmailCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'EmailCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const emailCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(EmailCarbonEmissionsCalculator, emailCarbonEmissions.address);
 	const emailCarbonEmissionsCalculator = await EmailCarbonEmissionsCalculator.deployed();
+	await emailCarbonEmissions.setCalculatorApproval(emailCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'LambCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'LambCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const lambCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(LambCarbonEmissionsCalculator, lambCarbonEmissions.address);
 	const lambCarbonEmissionsCalculator = await LambCarbonEmissionsCalculator.deployed();
+	await lambCarbonEmissions.setCalculatorApproval(lambCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'MilkCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'MilkCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const milkCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(MilkCarbonEmissionsCalculator, milkCarbonEmissions.address);
 	const milkCarbonEmissionsCalculator = await MilkCarbonEmissionsCalculator.deployed();
+	await milkCarbonEmissions.setCalculatorApproval(milkCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'MobileCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'MobileCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const mobileCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(MobileDataCarbonEmissionsCalculator, mobileCarbonEmissions.address);
 	const mobileDataCarbonEmissionsCalculator = await MobileDataCarbonEmissionsCalculator.deployed();
+	await mobileCarbonEmissions.setCalculatorApproval(mobileDataCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'PhoneCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'PhoneCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const phoneCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(PhoneCallCarbonEmissionsCalculator, phoneCarbonEmissions.address);
 	const phoneCallCarbonEmissionsCalculator = await PhoneCallCarbonEmissionsCalculator.deployed();
+	await phoneCarbonEmissions.setCalculatorApproval(phoneCallCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'PorkCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'PorkCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const porkCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(PorkCarbonEmissionsCalculator, porkCarbonEmissions.address);
 	const porkCarbonEmissionsCalculator = await PorkCarbonEmissionsCalculator.deployed();
+	await porkCarbonEmissions.setCalculatorApproval(porkCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'SalmonCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'SalmonCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const salmonCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(SalmonCarbonEmissionsCalculator, salmonCarbonEmissions.address);
 	const salmonCarbonEmissionsCalculator = await SalmonCarbonEmissionsCalculator.deployed();
+	await salmonCarbonEmissions.setCalculatorApproval(salmonCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'WalkCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'WalkCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const walkCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(WalkCarbonEmissionsCalculator, walkCarbonEmissions.address);
 	const walkCarbonEmissionsCalculator = await WalkCarbonEmissionsCalculator.deployed();
+	await walkCarbonEmissions.setCalculatorApproval(walkCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'YoutubeCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'YoutubeCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const youtubeCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(YoutubeCarbonEmissionsCalculator, youtubeCarbonEmissions.address);
 	const youtubeCarbonEmissionsCalculator = await YoutubeCarbonEmissionsCalculator.deployed();
+	await youtubeCarbonEmissions.setCalculatorApproval(youtubeCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'ElectricityCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'ElectricityCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const electricityCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(ElectricityCarbonEmissionsCalculator, electricityCarbonEmissions.address);
 	const electricityCarbonEmissionsCalculator = await ElectricityCarbonEmissionsCalculator.deployed();
+	await electricityCarbonEmissions.setCalculatorApproval(electricityCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'WaterCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'WaterCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const waterCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(WaterCarbonEmissionsCalculator, waterCarbonEmissions.address);
 	const waterCarbonEmissionsCalculator = await WaterCarbonEmissionsCalculator.deployed();
+	await waterCarbonEmissions.setCalculatorApproval(waterCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'GasCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'GasCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const gasCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(GasCarbonEmissionsCalculator, gasCarbonEmissions.address);
 	const gasCarbonEmissionsCalculator = await GasCarbonEmissionsCalculator.deployed();
+	await gasCarbonEmissions.setCalculatorApproval(gasCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'PublicTransportCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'PublicTransportCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const publicTransportCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(PublicTransportCarbonEmissionsCalculator, publicTransportCarbonEmissions.address);
 	const publicTransportCarbonEmissionsCalculator = await PublicTransportCarbonEmissionsCalculator.deployed();
+	await publicTransportCarbonEmissions.setCalculatorApproval(publicTransportCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'CarCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'CarCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const carCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(CarCarbonEmissionsCalculator, carCarbonEmissions.address);
 	const carCarbonEmissionsCalculator = await CarCarbonEmissionsCalculator.deployed();
+	await carCarbonEmissions.setCalculatorApproval(carCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'AirplaneCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'AirplaneCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const airplaneCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(AirplaneCarbonEmissionsCalculator, airplaneCarbonEmissions.address);
 	const airplaneCarbonEmissionsCalculator = await AirplaneCarbonEmissionsCalculator.deployed();
+	await airplaneCarbonEmissions.setCalculatorApproval(airplaneCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'TrainCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'TrainCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const trainCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(TrainCarbonEmissionsCalculator, trainCarbonEmissions.address);
 	const trainCarbonEmissionsCalculator = await TrainCarbonEmissionsCalculator.deployed();
+	await trainCarbonEmissions.setCalculatorApproval(trainCarbonEmissionsCalculator.address, true);
 
-	await deployer.deploy(CarbonEmissions, 'AmericanoCarbonEmissions');
+	await deployer.deploy(CarbonEmissions, 'AmericanoCarbonEmissions',process.env.EMISSION_NOTARY_PUBLIC);
 	const americanoCarbonEmissions = await CarbonEmissions.deployed();
 	await deployer.deploy(AmericanoCarbonEmissionsCalculator, americanoCarbonEmissions.address);
 	const americanoCarbonEmissionsCalculator = await AmericanoCarbonEmissionsCalculator.deployed();
+	await americanoCarbonEmissions.setCalculatorApproval(americanoCarbonEmissionsCalculator.address, true);
 
 	console.log('BeefCarbonEmissionsCalculator : ', beefCarbonEmissionsCalculator.address);
 	console.log('BicycleCarbonEmissionsCalculator : ', bicycleCarbonEmissionsCalculator.address);
