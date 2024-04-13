74cdf84e189cc0a308222437978b58ac5ec4e8ca
Jin Park <jinpk@users.noreply.github.com>
2024-04-13 21:25:52 +0900

    refactor: Comment for transferring default ioa eth

--- notary/response_nap.go
+++ notary/response_nap.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gesia-platform/core/context"
 	"github.com/gesia-platform/core/store"
@@ -17,7 +16,6 @@ import (
 func (notary *Notary) responseNetworkAccessPermission(ctx *context.Context, appID *big.Int, aggreatedSiganture []byte) error {
 	config := ctx.Config()
 	rootClient := ctx.ChainTree().Root.Client()
-	hostClient := ctx.ChainTree().Host.Client()
 
 	chainId := new(big.Int)
 	chainId.SetUint64(config.ChainTree.Root.ChainID)
@@ -33,11 +31,6 @@ func (notary *Notary) responseNetworkAccessPermission(ctx *context.Context, appI
 		panic(err)
 	}
 
-	txOpts, err := genTransactOpts(rootClient, chainId, config.ChainTree.PrivateKey)
-	if err != nil {
-		return err
-	}
-
 	// 1. PoA Genesis Node는 appID에 맵핑된 IOA를 생성하고 키 체인에 저장한다
 	pk, err := crypto.GenerateKey()
 	if err != nil {
@@ -49,9 +42,10 @@ func (notary *Notary) responseNetworkAccessPermission(ctx *context.Context, appI
 		fmt.Println("failed to set notary account to redis")
 		return err
 	}
-	fmt.Printf("notary account created appID: %s, address: %s\n", appID, ioaAddress.Hex())
+	fmt.Printf("IOA for appID: %s created. address: %s\n", appID, ioaAddress.Hex())
 
-	// 2. IOA에 선택된 Node가 기본 이더를 미리 송금해놓는다. 가스비 대납
+	/*// 2. IOA에 선택된 Node가 기본 이더를 미리 송금해놓는다. 가스비 대납
+	hostClient := ctx.ChainTree().Host.Client()
 	sendETHTxOpt, err := genTransactOpts(hostClient, hostChainId, config.ChainTree.PrivateKey)
 	if err != nil {
 		// 실패 시 삭제
@@ -85,10 +79,15 @@ func (notary *Notary) responseNetworkAccessPermission(ctx *context.Context, appI
 		return err
 	} else {
 		fmt.Printf("successfully send default coin to ioa tx: %s\n", tx.Hash())
-	}
+	}*/
 
 	// 3. app의 IOA 접근 권한을 허용한다.
-	tx, err = appPermission.ResponseNetworkAccess(txOpts, appID, notary.networkAccountAddress, aggreatedSiganture, true)
+	txOpts, err := genTransactOpts(rootClient, chainId, config.ChainTree.PrivateKey)
+	if err != nil {
+		return err
+	}
+
+	tx, err := appPermission.ResponseNetworkAccess(txOpts, appID, notary.networkAccountAddress, aggreatedSiganture, true)
 	if err != nil {
 		// 실패 시 삭제
 		ctx.Keychain().Del(basectx.Background(), appID.String())
