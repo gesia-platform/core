33d2affb69c03682943bf0ac58333818878f4558
winterm1 <oiool@naver.com>
2024-04-24 11:28:42 +0900

    feat(contract): add emission calculator

---
+++ apps/contract/contracts/carbon/emission/calculator/HamburgerCarbonEmissionsCalculator.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.0;
+
+import "../CarbonEmissions.sol";
+
+contract HamburgerCarbonEmissionsCalculator {
+    uint256 constant EMISSIONS_FACTOR = 3700000000; // scaled by 1,000,000,000
+
+    CarbonEmissions public immutable carbonEmissions;
+
+    constructor(CarbonEmissions _carbonEmissions)  {
+        carbonEmissions = _carbonEmissions;
+    }
+
+    function calculate(
+        uint256 applicationID,
+        bytes memory userID,
+        uint256 value
+    ) external returns (uint256) {
+        uint256 result = EMISSIONS_FACTOR * (value * 10e4) / 10e4;
+
+        carbonEmissions.mint(applicationID, result, userID);
+
+        return result;
+    }
+}
